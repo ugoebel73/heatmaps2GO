ae229d65b16bfbf76cffae7911dcec4815178419
Ulrike Göbel <ugoebel@uni-koeln.de>
2022-01-21 18:56:00 +0100

    Fri 21 Jan 2022 06:55:01 PM CET exploring why the proteasome genes are not found in the non-Enrichr heatmaps

--- heatmaps2GO.R
+++ heatmaps2GO.R
@@ -22,6 +22,13 @@ mart102 <- biomaRt::useMart(biomart="ENSEMBL_MART_ENSEMBL",
 GO_terms <- DBI::dbGetQuery(GO.db::GO_dbconn(), 
                             "SELECT go_id as GO_term, 
                              ontology as Ontology FROM go_term") 
+
+GO_synonyms <- as.list(GO.db::GOSYNONYM)
+names(GO_synonyms) <- sapply(GO_synonyms,function(x)x@GOID)
+
+## can get definitions from
+## as.list(GO.db::GOTERM)  <<<--- explore! 
+
 print(GO.db::GO.db) ## later also used to get parent/child relations
 ##GODb object:
 #  | GOSOURCENAME: Gene Ontology
@@ -113,53 +120,50 @@ biomaRt_v102_genes2GO$in_DGE <- biomaRt_v102_genes2GO$ENSEMBL %in% DGE_genes
 ## --------------------------------------------------------------------------
 ## "genes per GO term" lists for our_GO_terms by org.Mm.eq.db and by biomaRt:
 
-GO_genesets <- sapply(c("org", "biomaRt_v102"),
-                      function(var) {
-                        var <- paste0(var, "_genes2GO")
-                        lapply(list(all=TRUE,
-                                    dge=eval(as.symbol(var))$in_DGE),
-                                function(cnd) {
-                                    eval(as.symbol(var))[cnd,]             %>% 
-                                        filter(GO_term %in% our_GO_terms)  %>% 
-                                        select(-in_DGE)                    %>% 
-                                        nest_by(GO_term,
-                                                Ontology,.key="gene_set")
+GO2genes <- sapply(c("org", "biomaRt_v102"),
+                  function(var) {
+                    var <- paste0(var, "_genes2GO")
+                    lapply(list(all=TRUE,
+                                dge=eval(as.symbol(var))$in_DGE),
+                            function(cnd) {
+                                eval(as.symbol(var))[cnd,]              %>% 
+                                     filter(GO_term %in% our_GO_terms)  %>% 
+                                     select(-in_DGE)                    %>% 
+                                     nest_by(GO_term,
+                                             Ontology,.key="gene_set")
                        })
-               },simplify = FALSE) ## sapply sets the names -> simpler
+            },simplify = FALSE) ## sapply sets the names -> simpler
 
 ## note that nest_by sorts by GO term, 
-## so order is different "our_GO_terms"!
+## so the order is different from "our_GO_terms"!
 
 ## --------------------------------------------------------------------------
 ## accessor function to extract a column of the nested tibbles 
-## in GO_genesets[[]]:
+## in GO2genes[[]]:
 
 get_geneset_column <- function(tag="all",
-                               obj="GO_genesets$org",
+                               obj="GO2genes$org",
                                field="ENSEMBL") {
   l <- eval(parse(text=paste0('purrr::map(',obj,'[["',
                               tag,
                               '"]]$gene_set,function(x) x %>% dplyr::pull(',
                               field,
                               '))')))
-  names(l) <- GO_genesets[[tag]]$GO_term
+  names(l) <- GO2genes[[tag]]$GO_term
   l
 }  
 ## --------------------------------------------------------------------------
 ## Explore how gene sets defined by or.Mm.eg.de and biomaRt relate:
 
-setcounts <- tibble(GO_term=sort(our_GO_terms),
+GO_genesets <- tibble(GO_term=sort(our_GO_terms),
                     org_all=lapply(get_geneset_column(), unique),
                     org_dge=lapply(get_geneset_column(tag="dge"), unique),
                     biomaRt_all=lapply(get_geneset_column(
-                      obj = "GO_genesets$biomaRt_v102"), unique),
+                      obj = "GO2genes$biomaRt_v102"), unique),
                     biomaRt_dge=lapply(get_geneset_column(
                       tag="dge",
-                      obj = "GO_genesets$biomaRt_v102"),unique)
+                      obj = "GO2genes$biomaRt_v102"),unique)
                     )
-                      
-                    
-
 ##...........................................................................
 ## augment Ensembl v102 <-> GO associations  by the Ensembl v102 genes 
 ## associated with the children of each of "our_GO_terms" (as defined by GO.db):
@@ -170,21 +174,20 @@ child_terms <-  sapply(our_GO_terms,
                          more_specific_GOs(x,GO2entrez=org.Mm.egGO2EG)
                        },simplify=FALSE)
 
-setcounts$biomaRt_all_inclusive <- 
-       lapply(sort(our_GO_terms),
-              function(x) {
-                cnd <- biomaRt_v102_genes2GO$GO_term %in% child_terms[[x]]
-                unique(biomaRt_v102_genes2GO$ENSEMBL[cnd])
-              })
+for(tag in c("all","dge")) {
+  GO_genesets[[paste0("biomaRt_",tag,"_inclusive")]] <-
+        lapply(sort(our_GO_terms),
+               function(x) {
+                 ##browser()
+                 cnd <- biomaRt_v102_genes2GO$GO_term %in% child_terms[[x]]
+                 if(tag=="dge") {
+                   cnd <- cnd &  biomaRt_v102_genes2GO$in_DGE
+                 } 
+                 unique(biomaRt_v102_genes2GO$ENSEMBL[cnd])
+               })
+}
 
-setcounts$biomaRt_dge_inclusive <- 
-       lapply(sort(our_GO_terms),
-              function(x) {
-                cnd <- biomaRt_v102_genes2GO$in_DGE &
-                       (biomaRt_v102_genes2GO$GO_term %in% child_terms[[x]])
-                unique(biomaRt_v102_genes2GO$ENSEMBL[cnd])
-              })
-## > setcounts
+## > GO_genesets
 ## # A tibble: 10 × 7
 ## GO_term    org_all     org_dge     biomaRt_all biomaRt_dge biomaRt_all_inclusive biomaRt_dge_inclusive
 ## <chr>      <list>      <list>      <list>      <list>      <list>                <list>               
@@ -199,19 +202,156 @@ setcounts$biomaRt_dge_inclusive <-
 ##   9 GO:0071357 <chr [50]>  <chr [44]>  <chr [3]>   <chr [3]>   <chr [45]>            <chr [38]>           
 ##  10 GO:0071456 <chr [105]> <chr [92]>  <chr [119]> <chr [101]> <chr [142]>           <chr [122]>          
 
-## Note that lists with ~identical counts need not be identical (but similar):
-## > length(intersect(setcounts[[3,"org_all"]][[1]], setcounts[[3,"biomaRt_all_inclusive"]][[1]]))
+## Note that lists with ~identical counts need not be identical 
+## (but they do share a non-trivial core):
+## > length(intersect(GO_genesets[[3,"org_all"]][[1]], 
+##                    GO_genesets[[3,"biomaRt_all_inclusive"]][[1]]))
 ## [1] 62
-## > length(setcounts[[3,"org_all"]][[1]])
+## > length(GO_genesets[[3,"org_all"]][[1]])
 ## [1] 73
-## > length(setcounts[[3,"biomaRt_all_inclusive"]][[1]])
+## > length(GO_genesets[[3,"biomaRt_all_inclusive"]][[1]])
 ## [1] 74
 
 
-## > length(intersect(setcounts[[4,"org_all"]][[1]], setcounts[[4,"biomaRt_all_inclusive"]][[1]]))
+## > length(intersect(GO_genesets[[4,"org_all"]][[1]], 
+##                    GO_genesets[[4,"biomaRt_all_inclusive"]][[1]]))
 ## [1] 99
-## > length(setcounts[[4,"org_all"]][[1]])
+## > length(GO_genesets[[4,"org_all"]][[1]])
 ## [1] 130
-## > length(setcounts[[4,"biomaRt_all_inclusive"]][[1]])
+## > length(GO_genesets[[4,"biomaRt_all_inclusive"]][[1]])
 ## [1] 152
-##---------------------------------------------------------------------------
+## ..........................................................................
+## Where are the proteasome-related genes that are in the Enrichr heatmaps,
+## but no longer in the org.Mm.eg.db-based ones?
+load("DATA/groups_464.RData")
+proteasome_symbols <- groups_464[[3]]
+
+biomaRt_v102_genes2GO %>% 
+  filter((SYMBOL %in% proteasome_symbols) &
+         (ENSEMBL %in% Reduce(union,
+                              GO_genesets$biomaRt_all_inclusive)))
+## a single gene (same for "all" and "dge")
+## ENSMUSG00000005779  Psmb4
+
+org_genes2GO %>% 
+  filter((SYMBOL %in% proteasome_symbols) &
+           (ENSEMBL %in% Reduce(union,
+                                GO_genesets$org_all)))
+## the same single gene
+
+## ..........................................................................
+## Where is the Enrichr annotation coming from?
+
+load("/home/ugoebel/CECAD/Pipeline/Git/Heatmaps/heatmaps2GO_test/Reproduce/enrichr_proteasome_mapping.RData")
+enrichr_proteasome_mapping <- unlist(enrichr_proteasome_mapping) ## all length 1
+length(enrichr_proteasome_mapping)
+##[1] 25 
+
+enrichr_proteasome_GOs <- 
+all(enrichr_proteasome_mapping %in% DGE_genes)
+##[1] TRUE
+load("/home/ugoebel/CECAD/Pipeline/Git/Heatmaps/heatmaps2GO_test/Reproduce/enrichr_proteasome_GOs.RData")
+enrichr_proteasome_GOs <- sub("\\.",":",sub("\\s+","", enrichr_proteasome_GOs))
+##1] "GO:0070498:100%" "GO:0038061:100%" "GO:0033209:100%" "GO:0019221:100%"
+##[5] "GO:0071456:100%" "GO:0002479:96%"  "GO:0043312:28%"  "GO:0071357:4%"  
+##[9] "GO:0050727:4%"  
+
+all(sub(":\\d+%","",enrichr_proteasome_GOs) %in% GO_terms$GO_term)
+##[1] TRUE  ## should be TRUE, because this is a subset of our_GO_terms
+
+nrow(biomaRt_v102_genes2GO %>% filter(GO_term %in% 
+                                      sub(":\\d+%","",
+                                          enrichr_proteasome_GOs)) %>%
+                               distinct(ENSEMBL))
+##[1] 392
+g <- biomaRt_v102_genes2GO %>% filter(ENSEMBL %in% 
+                                      enrichr_proteasome_mapping)  %>%
+                                      distinct(ENSEMBL) %>% pull(ENSEMBL)
+length(g)
+##[1] 25 ## all 25 genes are actually represented
+
+table(g %in% DGE_genes) ## and they are all in our analysis set
+##TRUE 
+##  25 
+
+biomaRt_v102_genes2GO %>% filter(ENSEMBL %in% g) %>%
+                          distinct(SYMBOL) %>% pull(SYMBOL)
+##[1] "Psmc4"  "Psme1"  "Psme2"  "Psmb3"  "Psmb4"  "Psmb5"  "Psmd12" "Psma7" 
+##[9] "Psmd4"  "Psma5"  "Psmb2"  "Psmb6"  "Psmb7"  "Psma3"  "Psmb9"  "Psmf1" 
+##[17] "Psmb8"  "Psma6"  "Uba52"  "Psmd3"  "Psmc1"  "Psmc2"  "Psma2"  "Psmb10"
+##[25] "Psmb1" 
+
+t <- biomaRt_v102_genes2GO %>% filter(ENSEMBL %in% g) %>%
+                               distinct(GO_term) %>% pull(GO_term)
+length(unique(t))
+##[1] 97
+length(intersect(our_GO_terms,t))
+##[1] 0
+## the 25 genes are associated with 97 different GO terms, 
+## but not with any term of our_GO_terms
+
+length(which(t %in% names(GO_synonyms)))
+##[1] 22
+sapply(GO_synonyms[intersect(t, names(GO_synonyms))],
+       function(x) any(our_GO_terms %in% x@Synonym))
+#GO:0016887 GO:0030163 GO:0005515 GO:0008233 GO:0005839 GO:0004175 GO:0036064 
+#FALSE      FALSE      FALSE      FALSE      FALSE      FALSE      FALSE 
+#GO:0003674 GO:0006511 GO:0019882 GO:0003723 GO:0007519 GO:0003735 GO:0006412 
+#FALSE      FALSE      FALSE      FALSE      FALSE      FALSE      FALSE 
+#GO:0005840 GO:0022625 GO:0022627 GO:0030234 GO:0050790 GO:0016020 GO:0000902 
+#FALSE      FALSE      FALSE      FALSE      FALSE      FALSE      FALSE 
+#GO:0042098 
+#FALSE 
+sapply(GO_synonyms[intersect(t, names(GO_synonyms))],
+       function(x) any(our_GO_terms %in% x@Secondary))
+#GO:0016887 GO:0030163 GO:0005515 GO:0008233 GO:0005839 GO:0004175 GO:0036064 
+#FALSE      FALSE      FALSE      FALSE      FALSE      FALSE      FALSE 
+#GO:0003674 GO:0006511 GO:0019882 GO:0003723 GO:0007519 GO:0003735 GO:0006412 
+#FALSE      FALSE      FALSE      FALSE      FALSE      FALSE      FALSE 
+#GO:0005840 GO:0022625 GO:0022627 GO:0030234 GO:0050790 GO:0016020 GO:0000902 
+#FALSE      FALSE      FALSE      FALSE      FALSE      FALSE      FALSE 
+#GO:0042098 
+#FALSE 
+
+## like the information transfer was via
+## ENSEMBL from DGE (1)-> symbol (2)-> mapped to (?human) Enrichr symbol 
+##                  (3)-> associated with GO term in Enrichr database
+##                  (4)-> back-associated with original Ensembl ID
+##                        (implicitly by matrix row of origin)
+## after (1)->, the information on the original Ensembl ID is lost;
+## GO terms from other genes which happen to map to an (Enrichr flavor!) symbol 
+## will be transferred to the Ensembl ID.
+
+
+## see also GO.db.pdf under GOTERM:
+##All the obsolete GO terms are under the nodes "obsolete molecular function" (GO:0008369), "ob-
+##solete cellular component" (GO id GO:0008370), and "obsolete biological process" (GO:0008371).
+##Each of these GO identifiers has a group of GO identifiers as their direct children with GO terms
+##that were defined by GO but are deprecated in the current build. These deprecated GO terms were
+##appended by "(obsolete)" when the data package was built.
+##Mappings were based on data provided by: Gene Ontology http://current.geneontology.org/ontology/go-
+##  basic.obo With a date stamp from the source of: 2021-09-01
+## EXPLORE THIS <<<------------
+
+
+## copied from ws02:/data/public/ugoebel/Analysis/Test_Ensembl_versions
+## (should be 102 through biomaRt):
+load("DATA/biomart_descriptors_102.RData") ## varname: descriptors102
+
+length(unique(descriptors_102$ensembl_gene_id))
+##[1] 56305
+length(unique(biomaRt_v102_genes2GO$ENSEMBL))our_G
+##[1] 22302 ## much less .. like due to removed empty GO terms:
+
+tmp <- biomaRt::getBM(attributes = c("ensembl_gene_id",
+                                     "external_gene_name",
+                                     "go_id",
+                                     "go_linkage_type"),
+                      mart=mart102)
+length(unique(tmp$ensembl_gene_id))
+##[1] 56305
+length(unique(tmp$ensembl_gene_id[tmp$go_id!=""]))
+##[1] 22303
+
+
+## --------------------------------------------------------------------------
